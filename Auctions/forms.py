a1ddbbdbae2734266c96dd6d4a4516463eb01faa
Matheos Mattsson <matheos.mattsson@gmail.com>
2018-10-08 19:38:21 +0300

    Added Auctions form and started on new_auctions template and new urls /auctions/

--- Auctions/forms.py
+++ Auctions/forms.py
@@ -1,24 +1,27 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import *
 from django import forms
 from django.urls import reverse
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 
 
-class LoginForm(forms.ModelForm):
-    username = forms.CharField(label="Username", required=True)
-    password = forms.CharField(label="Password", widget=forms.PasswordInput, required=True)
+class AuctionForm(forms.ModelForm):
+
+    class Meta:
+        model = Auction
+        fields = ('title', 'description', 'min_price', 'deadline')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.helper = FormHelper
-        self.helper.form_action = reverse("login_user")
+        #self.helper.form_action = reverse("login_user")
         self.helper.form_method = "post"
-        self.helper.layout = Layout("username",
-                                    "password",
-                                    Submit("login", "Login", css_class="btn btn-success"))
+        self.helper.layout = Layout("title",
+                                    "description",
+                                    "min_price",
+                                    "deadline",
+                                    Submit("post", "Post", css_class="btn btn-success"))
 
 
 
--- Auctions/views.py
+++ Auctions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
+
+
+def new_auction(request):
+    if request.method == "POST":
+        form = AuctionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #return redirect("index")
+
+    else:
+        form = AuctionForm()
+
+    context = {"form": form}
+    return render(request, "new_auction.html", context)
